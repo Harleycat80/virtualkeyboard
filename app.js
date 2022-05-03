a50a2d4d8511a0da581377a3227091ec7b79b5da
Harleycat80 <96107085+Harleycat80@users.noreply.github.com>
2022-05-03 13:18:09 +0400

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", InsertTitle);
-function InsertTitle() {
-  const title = document.createElement("h1");
-  title.textContent = "Virtual KeyBoard";
-  document.body.append(title);
-}

0788bdc278a39b8119076b553a5ba6163914e133
Harleycat80 <Harleycat@rambler.ru>
2022-05-03 12:22:36 +0400

    init: start the project

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", InsertTitle);
+function InsertTitle(){
+  const title = document.createElement("h1");
+  title.textContent="Virtual KeyBoard";
+  document.body.append(title);
+
+
+}
+
+
+
